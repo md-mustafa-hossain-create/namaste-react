8f69efcf41247657cca75a0f8a82ce79952be7af
Md Mustafa Hossain <hussainmustafa2001@gmail.com>
2026-03-23 19:43:08 +0530

    feat: Introduce `useRestaurants` custom hook for fetching restaurant data, adding `axios` dependency and IDE ignore rules.

---
+++ food-ordering-app/src/utils/useRestaurants.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useRestaurants = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]); // Stores the list of restaurants being displayed after filtering/searching
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://namastedev.com/api/v1/listRestaurants",
+      );
+
+      const restaurants =
+        response?.data?.data?.data?.cards[1]?.card?.card?.gridElements
+          ?.infoWithStyle?.restaurants;
+
+      setListOfRestaurants(restaurants || []);
+      setFilteredRestaurants(restaurants || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { listOfRestaurants, filteredRestaurants, setFilteredRestaurants };
+};
+
+export default useRestaurants;
